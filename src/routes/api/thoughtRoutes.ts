850c050967ffd6516c9018fb74c1023c83a5da1c
Michael Henry Davies <michael_methodtheatre.com>
2025-04-23 08:10:00 -0600

    added README and fixed deleteReaction route

--- src/routes/api/thoughtRoutes.ts
+++ src/routes/api/thoughtRoutes.ts
@@ -9,7 +9,10 @@ router.route('/').get(getThoughts).post(createThought);
 router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
 
 // /api/thoughts/:thoughtId/reactions
-router.route('/:thoughtId/reactions').post(addReaction).delete(deleteReaction);
+router.route('/:thoughtId/reactions').post(addReaction);
+
+// api/thoughts/:thoughtId/reactions/:reactionId
+router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction);
 
 
 export default router;
